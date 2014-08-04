fb737c60f5dfe1530aaa958402cfba8a57f74e1d
Daniel Woelfel <dwwoelfel@gmail.com>
2014-08-04 15:41:14 -0700

    remove its tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -119,11 +119,6 @@ describe User do
     end
   end
 
-  describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
-  end
-
   describe "micropost associations" do
 
     before { @user.save }
@@ -146,27 +141,6 @@ describe User do
         expect(Micropost.where(id: micropost.id)).to be_empty
       end
     end
-
-    describe "status" do
-      let(:unfollowed_post) do
-        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-      end
-      let(:followed_user) { FactoryGirl.create(:user) }
-
-      before do
-        @user.follow!(followed_user)
-        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
-      end
-
-      its(:feed) { should include(newer_micropost) }
-      its(:feed) { should include(older_micropost) }
-      its(:feed) { should_not include(unfollowed_post) }
-      its(:feed) do
-        followed_user.microposts.each do |micropost|
-          should include(micropost)
-        end
-      end
-    end
   end
 
   describe "following" do
@@ -177,18 +151,11 @@ describe User do
     end
 
     it { should be_following(other_user) }
-    its(:followed_users) { should include(other_user) }
-
-    describe "followed user" do
-      subject { other_user }
-      its(:followers) { should include(@user) }
-    end
 
     describe "and unfollowing" do
       before { @user.unfollow!(other_user) }
 
       it { should_not be_following(other_user) }
-      its(:followed_users) { should_not include(other_user) }
     end
   end
 end
